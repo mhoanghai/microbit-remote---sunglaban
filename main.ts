f757cd2586aea2533c3b8e1f6e1b30f6a4c53ae7
mhoanghai <96908411+mhoanghai@users.noreply.github.com>
2021-12-31 16:28:54 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+bluetooth.onBluetoothConnected(function () {
+    basic.showIcon(IconNames.Yes)
+    ketnoi = 1
+})
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showIcon(IconNames.No)
+    ketnoi = 0
+})
+let ketnoi = 0
+bluetooth.startUartService()
+basic.showIcon(IconNames.Square)
+ketnoi = 0
+let datagui = ""
+basic.forever(function () {
+    if (ketnoi == 1) {
+        if (pins.digitalReadPin(DigitalPin.P0) == 0) {
+            datagui = convertToText(input.compassHeading())
+            bluetooth.uartWriteString(convertToText(input.compassHeading()))
+            basic.showIcon(IconNames.Heart)
+        } else {
+            bluetooth.uartWriteString(datagui)
+            basic.showIcon(IconNames.Square)
+        }
+    }
+})
